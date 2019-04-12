480d6ea8c52c6d7b0c7887868a418a0d2d1e9f2e
Dong Nguyen <nguyenvandong242@gmail.com>
2019-04-12 17:32:12 +0700

    cloudfone

--- test.php
+++ test.php
@@ -2,6 +2,11 @@
 $start_time = microtime(true);
 require_once 'includes/core.php';
 
-echo 'Count: '.$function->createParameter(array('page' => '{page}'));
+$num    = 268;
+$pagi   = 200;
+$plus   =  $num%$pagi;
+$pages  = ceil($num/$pagi);
+
+echo "$num - $pages";
 
 echo '<br />Load in '.(number_format(microtime(true) - $start_time, 2)).' seconds';
