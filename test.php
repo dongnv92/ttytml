58b4c5c194ffe1302aa478a2f8380b462f539f40
Dong Nguyen <nguyenvandong242@gmail.com>
2019-04-23 17:28:58 +0700

    edit tasks

--- test.php
+++ test.php
@@ -2,11 +2,7 @@
 $start_time = microtime(true);
 require_once 'includes/core.php';
 
-$num    = 268;
-$pagi   = 200;
-$plus   =  $num%$pagi;
-$pages  = ceil($num/$pagi);
+echo 'Hello World';
 
-echo "$num - $pages";
 
 echo '<br />Load in '.(number_format(microtime(true) - $start_time, 2)).' seconds';
